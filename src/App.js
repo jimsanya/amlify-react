ed57986e6e035c87eb26a4750a76e9a7aaf25260
James Osanya <jimsanya@yahoo.com>
2024-01-09 08:48:51 +0300

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { generateClient, Storage } from 'aws-amplify/api';
+import { generateClient } from 'aws-amplify/api';
+import { uploadData, getUrl, remove } from 'aws-amplify/storage';
 import {
   Button,
   Flex,
@@ -31,7 +32,7 @@ const App = ({ signOut }) => {
     await Promise.all(
       notesFromAPI.map(async (note) => {
         if (note.image) {
-          const url = await Storage.get(note.name);
+          const url = await getUrl({key: note.id});
           note.image = url;
         }
         return note;
@@ -49,7 +50,7 @@ const App = ({ signOut }) => {
       description: form.get("description"),
       image: image.name,
     };
-    if (!!data.image) await Storage.put(data.name, image);
+    if (!!data.image) await uploadData({key:data.name, data:image}).result;
     await client.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -61,7 +62,7 @@ const App = ({ signOut }) => {
   async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
-    await Storage.remove(name);
+    await remove({key:id});
     await client.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
